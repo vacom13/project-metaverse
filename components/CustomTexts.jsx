6bd7349ac3b8a18da5cb293f4ce79f65206f9fb9
Vansh Comar <69755265+vacom13@users.noreply.github.com>
2023-08-13 21:49:58 +0530

    About section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,19 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { textContainer, textVariant2 } from "../utils/motion";
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+export const TypingText = ({ title, textStyles }) => (
+  <motion.p
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+    {Array.from(title).map((Letter, index) => (
+      <motion.span key={index} variants={textVariant2}>
+        {Letter === " " ? "\u00A0" : Letter }
+      </motion.span>
+    ))}
+  </motion.p>
 );
 
-export const TitleText = () => (
-  <h2>Title Text</h2>
-);
+export const TitleText = () => <h2>Title Text</h2>;
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,8 +1,22 @@
-'use client';
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { TypingText, ExploreCard, TitleText } from "../components";
+import styles from "../styles";
+import { staggerContainer } from "../utils/motion";
+import { exploreWorlds } from "../constants";
 
 const Explore = () => (
-  <section>
-    Explore section
+  <section className={`${styles.paddings}`} id="explore">
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText />
+    </motion.div>
   </section>
 );
 
