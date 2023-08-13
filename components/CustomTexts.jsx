04e06ed5c17c11c33792d94e9566257fab487294
Vansh Comar <69755265+vacom13@users.noreply.github.com>
2023-08-13 23:06:52 +0530

    explore page

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -10,10 +10,19 @@ export const TypingText = ({ title, textStyles }) => (
   >
     {Array.from(title).map((Letter, index) => (
       <motion.span key={index} variants={textVariant2}>
-        {Letter === " " ? "\u00A0" : Letter }
+        {Letter === " " ? "\u00A0" : Letter}
       </motion.span>
     ))}
   </motion.p>
 );
 
-export const TitleText = () => <h2>Title Text</h2>;
+export const TitleText = ({ title, textStyles }) => (
+  <motion.h2
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`${textStyles} mt-[8px] font-bold md:text-[64px] text-[40px] text-white`}
+  >
+    {title}
+  </motion.h2>
+);
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
-'use client';
-import { motion } from "framer-motion";
+"use client";
 
-import styles from '../styles';
-import {navVariants} from '../utils/motion'
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
   <motion.nav
@@ -13,11 +13,19 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img src="/search.svg" alt="search" className="w-[24px] h-24[px] object-contain"/>
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white" >
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-24[px] object-contain"
+      />
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
         METAVERSUS
       </h2>
-      <img src="/menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
+      <img
+        src="/menu.svg"
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain"
+      />
     </div>
   </motion.nav>
 );
