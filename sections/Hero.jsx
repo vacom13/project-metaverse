62cc8f6fdda3268a5c0b34a98287d1a7a27fa652
Vansh Comar <69755265+vacom13@users.noreply.github.com>
2023-08-13 15:36:28 +0530

    Navbar and first commit

---
+++ sections/Hero.jsx
@@ -0,0 +1,12 @@
+'use client';
+import { motion } from "framer-motion";
+import{ styles } from '../styles';
+import { slideIn, staggerContainer,textVariant } from "../utils/motion";
+
+const Hero = () => (
+  <section>
+    Hero section
+  </section>
+);
+
+export default Hero;
